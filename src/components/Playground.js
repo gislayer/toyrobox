c14abf75643387a89e824d69cc1a25beeac459ba
GISLayer <alikilicharita@gmail.com>
2022-05-08 17:09:31 +0300

    designed for beeing mobile friendly

--- src/components/Playground.js
+++ src/components/Playground.js
@@ -9,18 +9,26 @@ export default class Playground extends Component {
                   (value) => {
                       debugger;
                       const {boxes} = value.game;
+                      var screenWidth = window.screen.availWidth;
+                      var boxHeight = 128;
+                      var boxWidth = '100%';
+                      if(screenWidth<=768){
+                        boxWidth = parseInt((screenWidth-40)/value.game.board.width);
+                        boxHeight = boxWidth;
+                      }
+                      
                       return(
                           <div className="cols">
                               {
                                   boxes.map((box)=>{
-                                    var boxName = (box.x+box.y)%2==1?'box1':'box2';
+                                    var boxName = (box.x+box.y)%2===1?'box1':'box2';
                                     if(box.robot===false && box.wall===false){
-                                        return(<div className={boxName}>{box.x}-{box.y}</div>)
+                                        return(<div style={{width:boxWidth,height:boxHeight}} className={boxName}>{box.x}-{box.y}</div>)
                                     }else{
-                                        if(box.wall==true){
-                                            return(<div className='wallbox'></div>)
+                                        if(box.wall===true){
+                                            return(<div style={{width:boxWidth,height:boxHeight}} className='wallbox'></div>)
                                         }else{
-                                            return(<div className={boxName}><img height="100" src={box.facing}/></div>)
+                                            return(<div style={{width:boxWidth,height:boxHeight}} className={boxName}><img height="100" alt="robot" src={box.facing}/></div>)
                                         }
                                     }
                                   })
